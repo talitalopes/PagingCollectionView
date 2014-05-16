8d26238ff05cf3a004532157c50e4523e805cfe8
Talita Lopes <talitalopes@gmail.com>
2014-05-15 21:50:23 -0300

    Incluindo carrouselcollectionview na viewcontroller

--- PagingCollectionView/ViewController.h
+++ PagingCollectionView/ViewController.h
@@ -7,7 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "CarrouselCollectionView.h"
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <CarrouselDelegate>
 
 @end
--- PagingCollectionView/ViewController.m
+++ PagingCollectionView/ViewController.m
@@ -10,6 +10,9 @@
 
 @interface ViewController ()
 
+@property (strong,nonatomic) CarrouselCollectionView *carousel;
+@property (strong,nonatomic) NSArray *data;
+
 @end
 
 @implementation ViewController
@@ -17,6 +20,13 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.data = @[@"Jogos Passados", @"Time que sigo", @"Jogos Futuros"];
+    
+    self.carousel = [[CarrouselCollectionView alloc] initWithFrame:CGRectMake(0, 64, 320, 40) andData:self.data];
+    self.carousel.carrouselDelegate = self;
+    [self.view addSubview:self.carousel];
+
+    
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
